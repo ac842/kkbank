988b0496321c255a72ee5dcc36e73e0c532abacf
ac842 <130188187+ac842@users.noreply.github.com>
2024-09-13 23:06:57 +0800

    Initial commit

--- src/pages/online.js
+++ src/pages/online.js
@@ -7,7 +7,7 @@ import Seo from "../components/seo"
 const OnlinePage = () => (
   <Layout>
     <h1>線上銀行</h1>
-    <p>KKBank 的安全便捷網上銀行服務，讓您隨時隨地管理您的財務。</p>
+    <p>NuoBank 的安全便捷網上銀行服務，讓您隨時隨地管理您的財務。</p>
     <ul>
       <li>帳戶查詢</li>
       <li>轉帳</li>
